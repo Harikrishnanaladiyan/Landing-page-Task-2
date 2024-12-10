fc0567cd40301fbd640675526bbafd508d3855f9
harikrishnan <harikrishnanaladiyan@gmail.com>
2024-12-10 21:43:04 +0530

    My second Landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily :{
+        'Righteous' :["Righteous","sans-serif"],
+        'Saira':["Saira", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
